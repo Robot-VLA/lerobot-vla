6d44ba2f5570409b3ce80be75940543d745788a6
Brandon Yang <jqm9ba@virginia.edu>
2025-07-28 09:33:25 +0000

    fixed beta time distribution bug.

--- src/lerobot/policies/pi0/modeling_pi0.py
+++ src/lerobot/policies/pi0/modeling_pi0.py
@@ -96,9 +96,8 @@ def create_sinusoidal_pos_embedding(
 
 
 def sample_beta(alpha, beta, bsize, device):
-    gamma1 = torch.empty((bsize,), device=device).uniform_(0, 1).pow(1 / alpha)
-    gamma2 = torch.empty((bsize,), device=device).uniform_(0, 1).pow(1 / beta)
-    return gamma1 / (gamma1 + gamma2)
+    m = torch.distributions.beta.Beta(torch.tensor([alpha]), torch.tensor([beta]))
+    return m.sample((bsize,)).to(device).reshape((bsize,))
 
 
 def make_att_2d_masks(pad_masks, att_masks):
