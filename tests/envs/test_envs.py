6007f320fdfd3d35c5f2eab2eaa34a8b3a1c1ae6
Brandon Yang <jqm9ba@virginia.edu>
2025-08-06 08:40:33 +0000

    fixed isaacsim close bug

--- tests/envs/test_envs.py
+++ tests/envs/test_envs.py
@@ -15,7 +15,7 @@ from torch import Tensor
 from lerobot.envs.factory import make_env, make_env_config
 from tests.utils import DEVICE
 
-AVAILABLE_ENV_NAMES = ["aloha", "pusht", "xarm", "maniskill"]
+AVAILABLE_ENV_NAMES = ["maniskill", "aloha", "pusht", "xarm"]
 
 
 class MockPolicyForEnv(nn.Module):
@@ -28,18 +28,31 @@ class MockPolicyForEnv(nn.Module):
         pass
 
     def select_action(self, batch: Dict[str, Tensor]) -> Tensor:
-        return torch.zeros((batch["observation.state"].shape[0], self.action_dim), dtype=torch.float32)
+        return torch.zeros(
+            (batch["observation.state"].shape[0], self.action_dim), dtype=torch.float32, device=DEVICE
+        )
 
 
 @pytest.mark.parametrize("env_name", AVAILABLE_ENV_NAMES)
-def test_all_envs(env_name):
+def test_all_envs(env_name, monkeypatch):
     num_envs = 5
-    seeds = np.random.randint(0, 1000, size=num_envs).tolist()
-    cfg = make_env_config(env_name)
-    env = make_env(cfg, n_envs=num_envs)
+    num_steps = 10
+    env = None
+
     try:
+        seeds = np.random.randint(0, 1000, size=num_envs).tolist()
+        cfg = make_env_config(env_name)
+        env = make_env(cfg, n_envs=num_envs)
+        monkeypatch.setattr(type(env), "_max_steps", property(lambda self: num_steps))
+
         mock_policy = MockPolicyForEnv(action_dim=cfg.features["action"].shape[0]).to(DEVICE)
         rollout_data = env.rollout(policy=mock_policy, seeds=seeds)
-        assert rollout_data is not None
+
+        assert rollout_data.action.shape == (num_envs, num_steps, cfg.features["action"].shape[0])
+        assert rollout_data.reward.shape == (num_envs, num_steps)
+        assert rollout_data.success.shape == (num_envs, num_steps)
+        assert rollout_data.done.shape == (num_envs, num_steps)
+
     finally:
-        env.close()
+        if env is not None:
+            env.close()
