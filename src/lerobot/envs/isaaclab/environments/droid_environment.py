3f8c8d3270c2a157eaa1e6b485be90a8dd474c95
Brandon Yang <jqm9ba@virginia.edu>
2025-09-03 03:56:05 +0000

    update asset paths and README instructions for IsaacSim integration

--- src/lerobot/envs/isaaclab/environments/droid_environment.py
+++ src/lerobot/envs/isaaclab/environments/droid_environment.py
@@ -23,7 +23,7 @@ from isaaclab.sensors import CameraCfg
 from isaaclab.utils import configclass, noise
 from pxr import Usd, UsdPhysics
 
-DATA_PATH = Path(os.getenv("ISAAC_SIM_ASSETS", "/isaac-sim/kit/cache/custom_assets"))
+DATA_PATH = Path(__file__).parent / "../../../../../assets"
 logging.info(f"Loading assets from: {DATA_PATH.resolve()}")  # noqa: F821
 
 
